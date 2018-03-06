15ef1b2a64fbbd1983e004acd63475bab1bf3d7f
ZhuSenlin <wumuzi520@126.com>
2018-03-07 02:29:43 +0800

    bugfix

--- FindFolly.cmake
+++ FindFolly.cmake
@@ -1,18 +1,18 @@
 #
 # Find FOLLY
 #
-#  FOLLY_INCLUDE_DIR - where to find folly/folly-config.h, etc.
-#  FOLLY_LIBRARY     - List of libraries when using libfolly.
+#  FOLLY_INCLUDE_DIRS - where to find folly/folly-config.h, etc.
+#  FOLLY_LIBRARIES     - List of libraries when using libfolly.
 #  FOLLY_FOUND       - True if libfolly found.
 
-FIND_PATH(FOLLY_INCLUDE_DIR folly/folly-config.h)
+FIND_PATH(FOLLY_INCLUDE_DIRS folly/folly-config.h)
 
-FIND_LIBRARY(FOLLY_LIBRARY NAMES folly)
+FIND_LIBRARY(FOLLY_LIBRARIES NAMES folly)
 
 # handle the QUIETLY and REQUIRED arguments and set FOLLY_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(FOLLY DEFAULT_MSG FOLLY_LIBRARY FOLLY_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(FOLLY DEFAULT_MSG FOLLY_LIBRARIES FOLLY_INCLUDE_DIRS)
 
-MARK_AS_ADVANCED(FOLLY_LIBRARY FOLLY_INCLUDE_DIR)
+MARK_AS_ADVANCED(FOLLY_LIBRARIES FOLLY_INCLUDE_DIRS)
 
